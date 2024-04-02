fbf1719bb5acc32d3685c70cb046586e9de0f106
nic <nickjemorris4@gmail.com>
2024-04-02 16:28:29 +0100

    Create field.py Model

---
+++ models/field.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, Double, Boolean, String, TIMESTAMP
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from app import database
+from models.crop import Crop
+
+
+class Field(database.Model):
+    id = Column(Integer, primary_key=True)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    number = Column(Integer, nullable=False, unique=True)
+
+    crops = relationship(Crop, backref='field', lazy=True)
+
+    ground_type = Column(String(255), nullable=True)
+    soil_type = Column(String(255), nullable=True)
+
+    nitrogen_level = Column(Integer, nullable=True)
+    ph_level = Column(Double, nullable=True)
+
+    plowed = Column(Boolean, nullable=True)
+    rolled = Column(Boolean, nullable=True)
+    weeded = Column(Boolean, nullable=True)
+    mulched = Column(Boolean, nullable=True)
+
