2303e87973626dd25c536c6a52a1912b5a5b7ee6
nic <nickjemorris4@gmail.com>
2025-03-10 22:32:56 +0000

    update db_models formatting

--- src/api/core/db_models.py
+++ src/api/core/db_models.py
@@ -33,25 +33,28 @@ class Farm(Repository):
     DB Model for farms.
     """
 
-    __tablename__ = "farms"
+    __tablename__ = 'farms'
 
     id = Column(UUID(), primary_key=True, default=uuid.uuid4)
     name = Column(String(255), nullable=False)
     description = Column(Text, nullable=True)
-    map = Column(String(255), nullable=True)
+    map_name = Column(String(255), nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC), nullable=False)
 
     owner_id = Column(UUID(), ForeignKey("users.id"), nullable=False)
+    map_id = Column(UUID(), ForeignKey("maps.id"), nullable=True)
 
     user = relationship("User", back_populates="farms")
+
     map = relationship("Map", back_populates="farms")
 
     def __repr__(self):
-        return f"<Farm: {self.name}, Map: {self.map}>"
+        return f"<Farm: {self.name}, Map: {self.map_name}>"
+
 
 class Map(Repository):
     """
-    DB Model for the Map
+    DB Model for Maps.
     """
 
     __tablename__ = "maps"
@@ -62,7 +65,5 @@ class Map(Repository):
     author = Column(String(100), nullable=True)
     release_date = Column(String(100), nullable=True)
     created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC), nullable=False)
-
-    farms = relationship("Farm", back_populates="map")
 
     farms = relationship("Farm", back_populates="map")
--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -3,9 +3,10 @@ Module for creating the FastAPI router
 """
 
 from fastapi import APIRouter
-from src.api.routes import auth_routes, user_routes, farm_routes
+from src.api.routes import auth_routes, user_routes, farm_routes, scrape_routes
 
 api_router = APIRouter()
 api_router.include_router(auth_routes.router)
 api_router.include_router(user_routes.router)
 api_router.include_router(farm_routes.router)
+api_router.include_router(scrape_routes.router)
