5c07b3cd374d90b8ff241b2f3fea5981d378587f
nic <nickjemorris4@gmail.com>
2025-03-10 22:08:53 +0000

    Create Mod pydantic model

--- src/api/core/models.py
+++ src/api/core/models.py
@@ -2,7 +2,9 @@ import datetime
 import uuid
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, Field
+
+from src.api.core.validators import Validators
 
 
 class LoginRequest(BaseModel):
@@ -64,3 +66,32 @@ class FarmsResponse(BaseModel):
 
     farms: list[FarmResponse]
     count: int
+
+
+class Mod(BaseModel):
+    """
+    Pydantic model for a Farming Simulator Mod Hub mod.
+    """
+
+    id: int
+    name: str
+    game: str = Field(..., alias="Game")
+    manufacturer: str = Field(..., alias="Manufacturer")
+    category: str = Field(..., alias="Category")
+    author: str = Field(..., alias="Author")
+    size: str = Field(..., alias="Size")
+    version: str = Field(..., alias="Version")
+    release_date: Optional[datetime.date | str] = Field(..., alias="Released")
+    platform: Optional[list | str] = Field(..., alias="Platform")
+
+    @field_validator("release_date")
+    def validate_release_date(cls, value):
+        return Validators.validate_release_date(value)
+
+    @field_validator("size")
+    def validate_size(cls, value):
+        return Validators.validate_size(value)
+
+    @field_validator("platform")
+    def validate_platform(cls, value):
+        return Validators.validate_platform(value)
