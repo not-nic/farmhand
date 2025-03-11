856a81daf144745ebf60665f392b0912cf3dfb81
nic <nickjemorris4@gmail.com>
2025-03-11 21:34:40 +0000

    update scrape route documentation

--- src/api/routes/scrape_routes.py
+++ src/api/routes/scrape_routes.py
@@ -13,9 +13,14 @@ router = APIRouter(prefix="/scrape", tags=["scraper"])
     status_code=status.HTTP_202_ACCEPTED
 )
 async def scrape_data(id: int, background_tasks: BackgroundTasks):
+    """
+    Function to manually trigger scraping of an individual mod by its mod_id.
+    :param id: the mod_id in the ModHub URL
+    :param background_tasks: The background task to add the scrape function too.
+    :return: (202) Accepted and a message that the scraping has been started in the background.
+    """
     mod_hub_service = ModHubService()
     background_tasks.add_task(mod_hub_service.scrape_mod, mod_id=id)
-
     return {"detail": "Started Scraping Task"}
 
 
@@ -26,8 +31,9 @@ async def scrape_data(id: int, background_tasks: BackgroundTasks):
 )
 async def scrape_maps(background_tasks: BackgroundTasks):
     """
-    :param background_tasks:
-    :return:
+    Function to manually trigger the scraping of maps from the Farming Simulator ModHub website.
+    :param background_tasks: The background task to add the scrape function too.
+    :return: (202) Accepted and a message that the scraping has been started in the background.
     """
     map_service = MapService()
     background_tasks.add_task(map_service.get_maps)
