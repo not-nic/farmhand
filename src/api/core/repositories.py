e0f5fe86bab844cfbbf52f4df5d20131e6465e0b
nic <nickjemorris4@gmail.com>
2025-03-16 03:00:48 +0000

    create fieldRepository

--- src/api/core/repositories.py
+++ src/api/core/repositories.py
@@ -1,11 +1,15 @@
 from typing import Optional, TypeVar, TYPE_CHECKING
 from uuid import UUID
+
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
+
+from src.api.constants import FarmTypes
 from src.api.core.db import Base, db_session
 
 
 if TYPE_CHECKING:
-    from src.api.core.db_models import User
+    from src.api.core.db_models import User, Field
 
 T = TypeVar("T", bound="Repository")
 
@@ -105,7 +109,7 @@ class UserRepository(Repository):
     __abstract__ = True
 
     @classmethod
-    def get_by_username(cls, username: str) -> Optional["User"]:
+    def get_by_username(cls: "User", username: str) -> Optional["User"]:
         """
         get a user by their username
         :param username:
@@ -114,10 +118,31 @@ class UserRepository(Repository):
         return cls.get_session().query(cls).filter(cls.username == username).first()
 
     @classmethod
-    def get_by_email(cls, email: str) -> Optional["User"]:
+    def get_by_email(cls: "User", email: str) -> Optional["User"]:
         """
         get a user by their email
         :param email: the users email
         :return: the user that matches the email, or none.
         """
         return cls.get_session().query(cls).filter(cls.email_address == email).first()
+
+
+class FieldRepository(Repository):
+    """
+    Field Repository for interacting with the DB and making queries
+    """
+
+    __abstract__ = True
+
+    @classmethod
+    def current_crop(cls: "Field"):
+        """
+        Get the most recent crop planted.
+        """
+        return cls.crops[0].crop if cls.crops else None
+
+    # def past_crops(self):
+    #     """
+    #     Get all previous crops (excluding the current one).
+    #     """
+    #     return [fc.crop for fc in self.crops[1:]]
