58dd995af3331ce068c51d824a80058b9c79f97b
nic <nickjemorris4@gmail.com>
2025-03-19 16:05:48 +0000

    add passlib[bcrypt] to requirements.txt and hide warning from logging config

--- src/api/utils.py
+++ src/api/utils.py
@@ -2,6 +2,9 @@ import logging
 import string
 import random
 
+from src.api.constants import FieldTypes, FarmTypes
+from src.api.core.db_models import Farm
+from src.api.core.models import FieldRequest
 from src.config import settings
 
 logger = logging.getLogger(__name__)
@@ -9,6 +12,9 @@ logger.setLevel(logging.INFO)
 
 log_handler = logging.StreamHandler()
 log_handler.setFormatter(logging.Formatter(settings.LOG_FORMATTER, datefmt="%Y-%m-%d %H:%M:%S"))
+
+# Passlib raises an error about not being able to find an attribute in bcrypt.
+logging.getLogger('passlib').setLevel(logging.ERROR)
 
 logger.addHandler(log_handler)
 
