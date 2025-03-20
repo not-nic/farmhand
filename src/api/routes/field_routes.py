847fea2ae200db183da20f7e518a3ada18ee6b96
nic <nickjemorris4@gmail.com>
2025-03-20 20:51:51 +0000

    Small refactor to field_routes.py

--- src/api/routes/field_routes.py
+++ src/api/routes/field_routes.py
@@ -13,6 +13,26 @@ router = APIRouter(prefix="/{id}/fields", tags=["Fields"])
 field_service = FieldService()
 
 
+@router.post("/", dependencies=[Depends(get_current_user)], status_code=status.HTTP_201_CREATED)
+async def create_field(
+        field_request: FieldRequest,
+        current_farm: Farm = Depends(get_users_farm)
+) -> Union[BaseGameFieldModel, PrecisionFarmingFieldModel]:
+    """
+    Create a field based on the current farm type.
+    Precision Farm's cannot create a 'Base' field and vice versa.
+    :param current_farm: the id of the farm in the request
+    :param field_request: the field request object.
+    """
+    try:
+        return field_service.create_field_by_field_type(field_request, current_farm)
+    except ValueError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(exc)
+        )
+
+
 @router.get("/", dependencies=[Depends(get_current_user)], status_code=status.HTTP_200_OK)
 async def get_fields(
     current_farm: Farm = Depends(get_users_farm),
@@ -31,26 +51,6 @@ async def get_fields(
     return FieldsResponse(fields=fields, count=fields_count).model_dump(exclude_none=True)
 
 
-@router.post("/", dependencies=[Depends(get_current_user)], status_code=status.HTTP_201_CREATED)
-async def create_field(
-    field_request: FieldRequest,
-    current_farm: Farm = Depends(get_users_farm)
-) -> Union[BaseGameFieldModel, PrecisionFarmingFieldModel]:
-    """
-    Create a field based on the current farm type.
-    Precision Farm's cannot create a 'Base' field and vice versa.
-    :param current_farm: the id of the farm in the request
-    :param field_request: the field request object.
-    """
-    try:
-        return field_service.create_field_by_field_type(field_request, current_farm)
-    except ValueError as exc:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(exc)
-        )
-
-
 @router.get(
     "/{field_id}",
     dependencies=[Depends(get_current_user), Depends(get_users_farm)],
@@ -62,4 +62,10 @@ async def get_field_by_id(field_id: UUID) -> Union[PrecisionFarmingFieldModel, B
     :param field_id: the id of the field
     :return: Pydantic PrecisionFarmingField or BaseFieldModel
     """
-    return field_service.get_field_details(field_id)
+    try:
+        return field_service.get_field_details(field_id)
+    except ValueError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(exc)
+        )
