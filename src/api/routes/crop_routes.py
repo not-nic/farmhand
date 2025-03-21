4a53312c9d9cbdb4f1b70fdc8d5ec5f2cb471866
nic <nickjemorris4@gmail.com>
2025-03-21 23:31:43 +0000

    create crop_routes.py and crop_service.py to plant crops, and filter field responses by crop type.

--- src/api/routes/crop_routes.py
+++ src/api/routes/crop_routes.py
@@ -1,6 +1,11 @@
+"""
+API Routes for Crops.
+"""
+from typing import Optional
+
 from fastapi import APIRouter, Depends, status
 
-from src.api.core.models import CropRequest
+from src.api.core.models import CropRequest, CropsResponse
 from src.api.deps import CurrentField, get_current_user, get_users_farm
 from src.api.services.crop_service import CropService
 
@@ -13,5 +18,39 @@ crop_service = CropService()
     dependencies=[Depends(get_current_user), Depends(get_users_farm)],
     status_code=status.HTTP_204_NO_CONTENT
 )
-async def plant_crop(crop_request: CropRequest, field: CurrentField) -> None:
-    crop_service.plant_crop(crop_request, field)
+async def plant_crop(field: CurrentField, crop_request: CropRequest) -> None:
+    """
+    Plant a crop in a field by providing a field and Crop and a Fround type.
+    :param crop_request:
+    :param field:
+    """
+    crop_service.plant_crop(current_field=field, crop_request=crop_request)
+
+
+@router.get(
+    "",
+    dependencies=[Depends(get_current_user), Depends(get_users_farm)],
+    status_code=status.HTTP_200_OK
+)
+async def get_crops(
+    field: CurrentField,
+    current: Optional[bool] = False,
+    past: Optional[bool] = False
+) -> CropsResponse:
+    """
+    Get crops planted in a field from the crop service and filter them by the possible queries.
+    :param field: the current field
+    :param current: the current crop planted in the field
+    :param past: the past crops that have been planted in the field.
+    :return: (CropsResponse) Pydantic model showing the id, crop_type and when it was planted.
+    """
+
+    if current:
+        return crop_service.get_current_crop(field)
+
+    if past:
+        return crop_service.get_past_crops(field)
+
+    return crop_service.get_all_crops(field)
+
+
