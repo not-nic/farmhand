e53afdca749a5647243a519b41ff0e02a886761e
nic <nickjemorris4@gmail.com>
2025-03-23 19:59:02 +0000

    Refactor JWT into security.py

--- src/api/core/security.py
+++ src/api/core/security.py
@@ -1,9 +1,23 @@
+"""
+Security module for handling various secure functions such as JWT encoding / decoding,
+OAuth configuration and password hashing.
+"""
+
+import jwt
+
+from typing import Optional
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from authlib.integrations.starlette_client import OAuth
 
+from src.api.constants import AuthTypes
+from src.api.core.db_models import User
+from src.api.core.models import TokenModel
 from src.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+reusable_oauth2 = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/login")
 
 oauth = OAuth()
 
@@ -43,3 +57,44 @@ class Security:
         :return: hashed password
         """
         return pwd_context.hash(password)
+
+    @staticmethod
+    def get_user_by_auth_type(token: TokenModel) -> Optional[User]:
+        """
+        Get a user based on the auth claim in the JWT Token.
+        :param token: the JWT token values
+        :return: (User) if it exists for the matching auth type.
+        """
+
+        if token.auth_type == AuthTypes.DEFAULT:
+            return User.get(token.id)
+        if token.auth_type == AuthTypes.GITHUB:
+            return User.get_by_github_id(token.id)
+        else:
+            raise ValueError("Unknown authentication type")
+
+    @staticmethod
+    def encode_jwt(payload: TokenModel) -> str:
+        """
+        Encode a JWT token with a TokenModel payload
+        :param payload: (TokenModel) of the data to encode
+        :return: (str) JWT token of encoded data.
+        """
+        return jwt.encode(
+            payload=payload.model_dump(mode="json"),
+            key=settings.JWT_SECRET_KEY,
+            algorithm=settings.JWT_ALGORITHM
+        )
+
+    @staticmethod
+    def decode_jwt(token: str) -> TokenModel:
+        """
+        Decode a JWT token back into a TokenModel.
+        :param token: (str) Encoded JWT token.
+        :return: (TokenModel) of the decoded JWT data.
+        """
+        try:
+            payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+            return TokenModel(**payload)
+        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+            raise
