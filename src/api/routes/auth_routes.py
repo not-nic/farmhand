539c11beef44cf48a0a2b117b32425c4e1199a07
nic <nickjemorris4@gmail.com>
2025-03-23 14:25:15 +0000

    (WIP) Github authentication endpoints

--- src/api/routes/auth_routes.py
+++ src/api/routes/auth_routes.py
@@ -1,14 +1,18 @@
-from fastapi import APIRouter, HTTPException, Depends, status, Response
-from datetime import datetime
+import jwt
+
+from fastapi import APIRouter, HTTPException, Depends, status, Request, Response
+from fastapi.responses import RedirectResponse
+import datetime
 
 from src.api.deps import get_current_user
-from src.api.core.models import LoginRequest
+from src.api.core.models import LoginRequest, GithubUser
 from src.api.core.db_models import User
-from src.api.core.security import Security
-from src.api.utils import generate_session_token
+from src.api.core.security import Security, github
+from src.api.utils import generate_session_token, logger
 from src.api.core.repositories import sessions
+from src.config import settings
 
-router = APIRouter(tags=["auth"])
+router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
@@ -32,7 +36,7 @@ async def login(login_request: LoginRequest, response: Response) -> dict:
         )
 
     session_token = generate_session_token()
-    sessions[session_token] = {"username": user.username, "created_at": datetime.utcnow()}
+    sessions[session_token] = {"username": user.username, "created_at": datetime.datetime.now(datetime.UTC)}
 
     response.set_cookie(key="session", value=session_token, httponly=True, secure=True)
 
@@ -55,3 +59,47 @@ async def logout(current_user: dict = Depends(get_current_user)) -> Response:
     response.delete_cookie("session")
 
     return response
+
+
+@router.get("/github")
+async def login(request: Request):
+    return await github.authorize_redirect(request, settings.GITHUB_OAUTH_CALLBACK_URL)
+
+
+@router.get("/github/callback")
+async def auth(request: Request):
+    token = await github.authorize_access_token(request)
+    response = await github.get("https://api.github.com/user", token=token)
+
+    github_user = GithubUser(**response.json())
+
+    logger.info(f"[Github Auth]: Logged in as {github_user.username}")
+
+    user = User.get_by_github_id(github_user.id)
+
+    # Create user if it does not exist
+    if not user:
+        logger.info(f"[Github Auth]: User {github_user.username} does not exist, creating new user...")
+        User.create(
+            github_id=github_user.id,
+            email=github_user.email,
+            username=github_user.username,
+            name=github_user.name
+        )
+
+    expiry_time = datetime.datetime.now(datetime.UTC) + settings.JWT_EXPIRATION_TIME
+
+    payload = {
+        "id": github_user.id,
+        "username": github_user.username,
+        "email": github_user.email,
+        "name": github_user.name,
+        "exp": expiry_time
+    }
+
+    session_token = jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+    response = RedirectResponse(url="/docs")
+    response.set_cookie(key="farmhand_user", value=session_token, httponly=True, secure=True)
+    return response
+
