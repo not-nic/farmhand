d8c00dc4233afc4d73131e79f6609bde580fe158
nic <nickjemorris4@gmail.com>
2025-03-24 20:40:38 +0000

    create tests for authenticating with github

--- tests/api/routes/test_auth_routes.py
+++ tests/api/routes/test_auth_routes.py
@@ -1,26 +1,68 @@
+"""
+Module for testing Farmhand authentication routes.
+"""
+
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 from src.config import settings
 
 
 class TestAuthRoutes:
-    url = f"{settings.API_V1_STR}/auth/login"
+    url = f"{settings.API_V1_STR}/auth"
 
     @staticmethod
     def post(url: str, data: dict, client: TestClient):
         return client.post(url, json=data)
 
+    @staticmethod
+    def get(url: str, client: TestClient):
+        return client.get(url)
+
     def test_login_without_credentials(self, client):
+        """
+        Test logging into the Farmhand service with a username and password
+        with incorrect credentials and assert a 401 error is returned.
+        :param client: FastAPI client
+        """
         payload = {"username": "notauser", "password": "notapassword"}
 
-        response = self.post(url=self.url, data=payload, client=client)
+        response = self.post(url=f"{self.url}/login", data=payload, client=client)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
         assert response.json() == {"detail": "Username or password is incorrect"}
 
-    def test_login_with_credentials(self, client, create_test_user):
+    def test_login_with_credentials(self, client, unit_test_user):
+        """
+        Test logging into the Farmhand service with a username and password
+        with the correct credentials and asset a 200 success.
+        :param client: FastAPI client
+        :param unit_test_user: the unit test user fixture
+        """
         payload = {"username": "unit-testing-user", "password": "unit-testing-password"}
 
-        response = self.post(url=self.url, data=payload, client=client)
+        response = self.post(url=f"{self.url}/login", data=payload, client=client)
 
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == {"message": "login successful"}
+
+    @pytest.mark.asyncio
+    async def test_login_with_github(self, client, mock_github_login, github_user):
+        """
+        test logging in with GitHub authentication
+        :param client: FastAPI test client
+        :param github_user: GitHub user fixture
+        """
+        response = self.get(f"{self.url}/github", client)
+        mock_github_login.assert_called_once()
+        assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
+
+    @pytest.mark.asyncio
+    async def test_github_callback_existing_user(self, client, mock_github_authentication, github_user):
+        """
+        Test github callback and mock logging in a github user.
+        :param client: FastAPI test client
+        :param client: FastAPI test client
+        :param github_user: GitHub user fixture
+        """
+        response = self.get(f"{self.url}/github/callback", client)
+        assert response.status_code == status.HTTP_200_OK
