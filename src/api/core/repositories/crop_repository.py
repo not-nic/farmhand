ae4d120bb5a5a41a7e75f2e080ee9af82d29993b
nic <nickjemorris4@gmail.com>
2025-03-30 16:09:09 +0100

    refactor repositories.py into a module

---
+++ src/api/core/repositories/crop_repository.py
@@ -0,0 +1,27 @@
+"""
+Crop Repository containing crop database interactions.
+see: base_repository.py to see the base repository to inherit from.
+"""
+
+from typing import Optional, TYPE_CHECKING
+from src.api.core.repositories.base_repository import Repository
+
+if TYPE_CHECKING:
+    from src.api.core.db_models import Crop
+
+
+class CropRepository(Repository):
+    """
+    User Repository for interaction with the DB
+    """
+
+    __abstract__ = True
+
+    @classmethod
+    def get_by_type(cls: "Crop", type: str) -> Optional["Crop"]:
+        """
+        Get a crop by its type / plaintext name e.g. Wheat.
+        :param type: the type of crop.
+        :return: the crop object.
+        """
+        return cls.get_session().query(cls).filter(cls.type == type).first()
