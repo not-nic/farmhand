fa40ad5082f2e7bc369f8e3a100f985ca97df513
nic <nickjemorris4@gmail.com>
2025-06-08 20:12:25 +0100

    unit tests for create, get and filter

---
+++ tests/api/services/test_task_service.py
@@ -0,0 +1,106 @@
+"""
+Task Service unit tests
+"""
+from uuid import UUID
+
+import pytest
+
+from src.api.core.db.models import Task
+from src.api.services.tasks_service import TaskService
+
+
+@pytest.mark.usefixtures("create_database", "unit_test_user")
+class TestTaskService:
+
+    @pytest.fixture
+    def tasks(self, farm):
+        """Pytest tasks fixture"""
+        task_service = TaskService()
+        task_service.create_task("new task data 1", completed=False, farm_id=farm.id)
+        task_service.create_task("new task data 2", completed=True, farm_id=farm.id)
+        task_service.create_task("new task data 3", completed=False, farm_id=farm.id)
+        return farm.tasks
+
+    def test_create_task(self, farm):
+        """
+        Test that a task can be created and when retrieved
+        it is the same as the created task.
+        :param farm: Farm Fixture.
+        """
+        task_service = TaskService()
+        new_task = task_service.create_task("new task data", completed=False, farm_id=farm.id)
+        db_task = Task.get(new_task.id)
+
+        assert new_task is db_task
+
+    def test_create_task_with_too_much_content(self, farm):
+        """
+        Test that a ValueError is raised when a task's content is > MAX_TASK_LENGTH
+        :param farm: Farm Fixture.
+        """
+        with pytest.raises(ValueError, match="Tasks must be shorter than '280' characters."):
+            task_service = TaskService()
+            task_service.create_task(
+                content=("A" * 281),
+                completed=False,
+                farm_id=farm.id
+            )
+
+    def test_get_tasks(self, farm, tasks):
+        """
+        Test that multiple tasks can be retrieved.
+        :param farm: Farm Fixture.
+        :param tasks: Tasks Fixture.
+        """
+        task_service = TaskService()
+
+        task_service.create_task(
+            "different farm task",
+            completed=False,
+            farm_id=UUID("3ba72831-2faa-4fae-87b2-6a4cbab16b34")
+        )
+
+        tasks = task_service.get_tasks(farm)
+
+        assert len(tasks) == 3
+
+    def test_filter_complete_tasks(self, farm, tasks):
+        """
+        Test that multiple tasks can be retrieved and filtered to only 'complete' tasks.
+        :param farm: Farm Fixture.
+        :param tasks: Tasks Fixture.
+        """
+        task_service = TaskService()
+        tasks = task_service.get_tasks(farm, filter_by="complete")
+        assert len(tasks) == 1
+
+    def test_filter_incomplete_tasks(self, farm, tasks):
+        """
+        Test that multiple tasks can be retrieved and filtered to only 'incomplete' tasks.
+        :param farm: Farm Fixture.
+        :param tasks: Tasks Fixture.
+        """
+        task_service = TaskService()
+        tasks = task_service.get_tasks(farm, filter_by="incomplete")
+        assert len(tasks) == 2
+
+    def test_get_task_by_id(self, farm):
+        """
+        Test getting a task by its ID.
+        :param farm: Farm Fixture.
+        :return:
+        """
+        task_service = TaskService()
+        task = task_service.create_task("new task data", completed=False, farm_id=farm.id)
+        db_task = task_service.get_task_by_id(task.id)
+
+        assert task is db_task
+
+    def test_update_task(self, farm):
+        pass
+
+    def test_complete_task(self, farm):
+        pass
+
+    def test_delete_task(self, farm):
+        pass
