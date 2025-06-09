ae2a2012eb65deb67a286354e2ef4b9cfc744d01
nic <nickjemorris4@gmail.com>
2025-06-09 18:56:00 +0100

    create validation error exception handler

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,6 +1,10 @@
 """
 Farmhand util functions.
 """
+from typing import Union
+
+from fastapi.exceptions import RequestValidationError
+from pydantic import ValidationError
 
 
 def parse_version(v: str) -> list:
@@ -10,3 +14,16 @@ def parse_version(v: str) -> list:
     :return: a list of the integer parts.
     """
     return [int(part) for part in v.split('.')]
+
+
+def format_pydantic_errors(validation_error: Union[ValidationError | RequestValidationError]) -> dict:
+    """
+    Util function to nicely format pydantic validation errors.
+    :param validation_error: Pydantic ValidationError
+    :return: (dict) error message 'detail' response
+    """
+    errors = validation_error.errors()
+    if errors:
+        messages = [err.get("msg", "Validation error") for err in errors]
+        return {"detail": "; ".join(messages)}
+    return {"detail": "Unknown validation error"}
