6fd88b448e90b300816683f368edbaf20d7a9554
nic <nickjemorris4@gmail.com>
2025-06-21 02:18:01 +0100

    add new repositories to __init__.py

--- src/api/core/repositories/__init__.py
+++ src/api/core/repositories/__init__.py
@@ -5,6 +5,19 @@ init module to import each repository into.
 from .user_repository import UserRepository
 from .base_repository import Repository
 from .field_repository import FieldRepository
+from .field_crop_repository import FieldCropRepository
 from .crop_repository import CropRepository
+from .farm_repository import FarmRepository
+from .map_repository import MapRepository
+from .task_repository import TaskRepository
 
-__all__ = ["UserRepository", "Repository", "FieldRepository", "CropRepository"]
+__all__ = [
+    "UserRepository",
+    "Repository",
+    "FieldRepository",
+    "FarmRepository",
+    "FieldCropRepository",
+    "CropRepository",
+    "MapRepository",
+    "TaskRepository"
+]
