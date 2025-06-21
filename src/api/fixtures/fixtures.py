267a4109fa4a5da9f1dcd9d1e41ca5a1ab6dc6b5
nic <nickjemorris4@gmail.com>
2025-06-21 02:21:39 +0100

    add db session context to start up fixtures

--- src/api/fixtures/fixtures.py
+++ src/api/fixtures/fixtures.py
@@ -3,7 +3,9 @@ Module for initialisation scripts (fixtures) to be run
 on the start-up of the application.
 """
 
+from src.api.core.db.db_setup import get_db, db_session
 from src.api.core.db.models.users import User
+from src.api.core.repositories.user_repository import UserRepository
 from src.api.core.security import Security
 from src.api.services.crop_service import CropService
 from src.api.core.logger import logger
@@ -26,22 +28,25 @@ class Fixtures:
             "name": "service-user",
         }
 
-        service_user = User.get_by_username(username=settings.SERVICE_USER_USERNAME)
+        with db_session() as session:
+            user_repository = UserRepository(session)
+            service_user = user_repository.get_by_username(username=settings.SERVICE_USER_USERNAME)
 
-        if not service_user:
-            logger.info("Creating service user...")
-            service_user = User.create(**user_data)
-            logger.info(f"Service user created - {service_user.email_address}")
-        else:
-            logger.info("Service user already exists, updating values if changed.")
-            service_user.update(id=service_user.id, **user_data)
+            if not service_user:
+                logger.info("Creating service user...")
+                service_user = user_repository.create(**user_data)
+                logger.info(f"Service user created - {service_user.email_address}")
+            else:
+                logger.info("Service user already exists, updating values if changed.")
+                user_repository.update(id=service_user.id, **user_data)
 
-        return service_user
+            return service_user
 
     @staticmethod
     async def create_crop_data() -> None:
         """
         Python Fixture for creating crop data on application startup if it does not exist.
         """
-        crop_service = CropService()
-        await crop_service.load_crops()
+        with db_session() as session:
+            crop_service = CropService(session)
+            await crop_service.load_crops()
